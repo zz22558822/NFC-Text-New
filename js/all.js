449ddd689d852caf7182af3486d0ce6ede51a521
Chek <79972532+zz22558822@users.noreply.github.com>
2023-06-26 10:59:06 +0800

    V3

--- js/all.js
+++ js/all.js
@@ -1,6 +1,7 @@
 const infoBox = document.querySelector('.infoBox');
-const SN = document.querySelector("#SN").value;
-const Num = document.querySelector("#Num").value
+let SN = document.querySelector("#SN");
+let Num = document.querySelector("#Num");
+let reader = null; // 儲存 NDEFReader 物件
 
 // 新增文字資訊框調用
 function addText(event, className) {
@@ -18,11 +19,16 @@ function startNfcScan() {
     // 取得狀態元素
     const statusElement = document.getElementById('status');
 
-    // 建立 NDEFReader 物件
-    const reader = new NDEFReader();
+    // 移除已有的讀取事件監聽器
+    if (reader !== null) {
+      reader.removeEventListener('reading', handleNfcReading);
+    }
 
-    // 監聽 'reading' 事件
-    reader.addEventListener('reading', event => {
+    // 建立 NDEFReader 物件
+    reader = new NDEFReader();
+
+    // 定義讀取事件的處理函數
+    function handleNfcReading(event) {
       // 取得訊息中的記錄
       const records = event.message.records;
 
@@ -36,14 +42,17 @@ function startNfcScan() {
       const serialNumber = event.serialNumber;
       console.log(`> 序號: ${serialNumber}`); // 輸出訊息，顯示序列號
       addText(`> 序號: ${serialNumber}`, 'bold');
-	  SN = serialNumber
+      SN.value = serialNumber;
 
       // 顯示記錄數量
       const numberOfRecords = event.message.records.length;
       console.log(`> 記錄: (${numberOfRecords})`); // 輸出訊息，顯示記錄數量
       addText(`> 記錄: (${numberOfRecords})`, 'bold');
-	  Num = numberOfRecords
-    });
+      Num.value = numberOfRecords;
+    }
+
+    // 監聽 'reading' 事件
+    reader.addEventListener('reading', handleNfcReading);
 
     // 在掃描開始前更新狀態元素為「掃描中」
     statusElement.innerHTML = '<i class="fas fa-spinner fa-spin"></i> NFC 狀態：掃描中';
@@ -67,40 +76,3 @@ function startNfcScan() {
 
   addText('', 'bt');
 }
-
-
-  
-
-// // 檢查瀏覽器是否支援NFC功能
-// 	if (!("NDEFReader" in window)) {
-// 		console.log("瀏覽器不支援NFC功能"); // 輸出訊息，表示瀏覽器不支援NFC功能
-// 		scanButton.disabled = true; // 禁用掃描按鈕
-
-// }
-
-// // 監聽掃描按鈕的點擊事件
-// scanButton.addEventListener("click", async () => {
-// 	console.log("使用者按下了掃描按鈕"); // 輸出訊息，表示使用者按下了掃描按鈕
-
-  
-// 	try {
-// 	  const ndef = new NDEFReader(); // 創建 NDEFReader 物件
-// 	  await ndef.scan(); // 開始掃描NFC標籤
-// 	  console.log("> 掃描已開始"); // 輸出訊息，表示掃描已開始
-  
-// 	  // 監聽讀取錯誤事件
-// 	  ndef.addEventListener("readingerror", () => {
-// 		console.log("無法從NFC標籤讀取數據，請嘗試另一個標籤"); // 輸出訊息，表示無法從NFC標籤讀取數據，建議使用者嘗試另一個標籤
-// 	  });
-  
-// 	  // 監聽讀取事件
-// 	  ndef.addEventListener("reading", ({ message, serialNumber }) => {
-// 		console.log(`> 序號: ${serialNumber}`); // 輸出訊息，顯示序列號
-// 		console.log(`> 記錄: (${message.records.length})`); // 輸出訊息，顯示記錄數量
-// 	  });
-// 	} catch (error) {
-// 		console.log("錯誤! " + error); // 輸出錯誤訊息
-// 	}
-//   });
-
-
